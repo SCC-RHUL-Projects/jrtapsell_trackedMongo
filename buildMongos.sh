1bb777bf4aad9c74b373a5288a2722905ab166a8
James Tapsell <email@jrtapsell.co.uk>
2018-07-10 14:00:01 +0100

    Updated build

--- buildMongos.sh
+++ buildMongos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source venv/bin/activate
 cd mongo
-time ./buildscripts/scons.py mongos mongod CC=/usr/bin/gcc-5 CXX=/usr/bin/g++-5 -j 4 --disable-warnings-as-errors
+time ./buildscripts/scons.py mongos mongod CC=/usr/bin/gcc-5 CXX=/usr/bin/g++-5 -j 16 --disable-warnings-as-errors
 deactivate
 ./notify.sh "Build complete"
 
