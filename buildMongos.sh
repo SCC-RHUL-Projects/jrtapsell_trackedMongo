d533208d25c5422636778b244b0dfd70aef981c7
James Tapsell <email@jrtapsell.co.uk>
2018-07-10 14:02:33 +0100

    Added pop

--- buildMongos.sh
+++ buildMongos.sh
@@ -3,5 +3,6 @@ source venv/bin/activate
 cd mongo
 time ./buildscripts/scons.py mongos mongod CC=/usr/bin/gcc-5 CXX=/usr/bin/g++-5 -j 16 --disable-warnings-as-errors
 deactivate
+cd ../
 ./notify.sh "Build complete"
 
