25e588607776f1f3e20946cb14b790fc764c0ce7
James Tapsell <email@jrtapsell.co.uk>
2018-07-15 21:31:15 +0100

    Updated to strip and remove old images

--- buildImage.sh
+++ buildImage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#strip --strip-all mongo/mongo*
+strip --strip-all mongo/mongo*
+docker images purge
 cp mongo/mongod docker
 cp mongo/mongos docker
 docker build -t mongo_patched docker
