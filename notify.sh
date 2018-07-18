3cf149394f060bae6390e176bfbe587695dce0d8
James Tapsell <email@jrtapsell.co.uk>
2018-07-18 08:59:20 +0100

    Changed to notify on the main slack channel

--- notify.sh
+++ notify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$1\"}" https://hooks.slack.com/services/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 > /dev/null 2>&1
+curl dice-zcva821-02:8888 -X POST -d "$1"
