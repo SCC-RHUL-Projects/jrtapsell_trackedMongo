447dafe2c460989d979cd8c95970b1ef333a00a8
James Tapsell <email@jrtapsell.co.uk>
2018-07-19 17:33:03 +0100

    Updated to notify in a better way

--- all.sh
+++ all.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+../notify.sh "Run started at $(date)"
 git pull && cd mongo && git pull && cd ../cluster && git pull && cd ../ && ./buildMongos.sh && ./buildImage.sh && ./launch.sh
+../notify.sh "Run completed at $(date)"
