b8bb596b4323ca9b8e7aae431a2ba7fba24fce55
James Tapsell <email@jrtapsell.co.uk>
2018-07-20 17:02:38 +0100

    Updated to try to handle build failure more gracefully

--- all.sh
+++ all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 ./notify.sh "Run started at $(date)"
 git pull && cd mongo && git pull && cd ../cluster && git pull && cd ../ && ./buildMongos.sh && ./buildImage.sh && ./launch.sh
 ./notify.sh "Run completed at $(date)"
--- buildMongos.sh
+++ buildMongos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap 'exit 1' ERR
 source venv/bin/activate
 cd mongo
 time ./buildscripts/scons.py mongos mongod CC=/usr/bin/gcc-5 CXX=/usr/bin/g++-5 -j 16 --disable-warnings-as-errors
